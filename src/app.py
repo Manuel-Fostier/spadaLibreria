b7302eae5648bf42445afddb6c4dd0bfa7ea8c86
Manuel Fostier <manuel.fostier@gmail.com>
2025-06-10 20:11:28 +0000

    ligne decommenté

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 import os
 from pathlib import Path
-#from database import DatabaseManager
+from database import DatabaseManager
 
 #TODO : reader tutorial : https://fastapi.tiangolo.com/tutorial/first-steps/#check-the-openapijson
 
@@ -15,23 +15,17 @@ current_dir = Path(__file__).parent
 app.mount("/static", StaticFiles(directory=os.path.join(Path(__file__).parent,
           "static")), name="static")
 
-# db_manager = DatabaseManager()
-# db_manager.connect()
-# db_manager.create_tables()
+db_manager = DatabaseManager()
+db_manager.connect()
+db_manager.create_tables()
 
 @app.get("/")
 def root():
     return RedirectResponse(url="/static/index.html")
 
-@app.get("/health")
-def health():
-    return {"status": "ok"}
 
 # @app.get("/search")
 # def search_entries(regex: str = Query(...), language: str = Query("fr")):
 #     results = db_manager.search_entries(regex, language=language)
 #     # On retourne une liste de chaînes (par exemple, le champ texte)
 #     return {"results": [r[1] for r in results]}
-
-async def root():
-    return {"message": "Hello World"}
